bc7bed7e1c874cb78905bcef6821c66475a83896
Jason Huang <jasonhlx1213@gmail.com>
2020-09-10 00:51:06 +0300

    Updates

---
+++ precache-manifest.53e87f5a28f0691038829f4b45704cf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be71735de2aa85e0aa60477ec454111b",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "b3df9aff31378d7ed920",
+    "url": "/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a9d67a066032985e0a50",
+    "url": "/face-recognition/static/css/main.40ad4f82.chunk.css"
+  },
+  {
+    "revision": "b3df9aff31378d7ed920",
+    "url": "/face-recognition/static/js/2.1240cd3a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition/static/js/2.1240cd3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d67a066032985e0a50",
+    "url": "/face-recognition/static/js/main.48c17968.chunk.js"
+  },
+  {
+    "revision": "b86f4757c9b31828b647",
+    "url": "/face-recognition/static/js/runtime-main.9817fe0c.js"
+  }
+]);
